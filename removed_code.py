f9496f0564df85f7c0beb95c8ecb8069d9652d86
mrb <93202187+bureata@users.noreply.github>
2023-03-04 22:16:51 +0200

    - moved "elemente_extras.py" content into "removed_code.py" - removed "elemente_extras.py"

--- removed_code.py
+++ removed_code.py
@@ -1,4 +1,25 @@
-from elemente_extras import *
+lungime_rand = 100
+rand_1 = 'MBBank                                                             Raport tranzactii pentru perioada'
+nr_spatii_rand_2 = 79
+nr_caractere_fixe_rand_antet = 45
+pozitie_coloana2_date_client = 56
+pozitie_detalii_cap_tabel = 26
+pozitie_sfarsit_debit = 81
+cap_tabel = 'Data                     Detalii tranzactie                                 Debit             Credit'
+spatii_data_tranzactie = 7
+spatii_detalii_tranzactie = 25
+
+rand_acronim = 'MBBank'
+rand_sold_initial = 'Sold initial:            '
+rand_sold_final = 'Sold final:              '
+footer = """
+Andrada Perlea             Semnatura                             Mirela Ilie            Semnatura
+
+Sef Serviciu Dezvoltare Produse                                  Sef Serviciu Relatii Clienti
+MBBank                                                           MBBank
+Sucursala Bucuresti                                              Sucursala Bucuresti
+"""
+
 
 def constructor_extras(nume_client, clients_collection):
     date_client = clients_collection.find_one({"name": nume_client}, {"_id": 1, "adresa": 1})
@@ -92,3 +113,5 @@ def tranzactii_in_perioada(nume_client, clients_collection):
 
     perioada = (formatere_data_antet(data_inceput), formatere_data_antet(data_sfarsit))
     return lista_tranzactii, perioada
+
+
